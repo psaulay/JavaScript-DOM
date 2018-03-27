56ac0dac1f9d9d6bf2831239e90243b306d7c7e2
psaulay <psaulay@gmail.com>
2018-03-27 14:49:30 +0200

    delete error folder

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ function Add10px(){
     var taille = x.offsetHeight; 
     //si var taille < 300px
     if (taille < 300) {
-    // ajouter 10px 
-    x.style.height = taille + 10 + 'px';
-    return}
+        // ajouter 10px 
+        x.style.height = taille + 10 + 'px';
+        return}
     //sinon revenir à 100px
     x.style.height = 100 + 'px';
-  
 }
 
 function Green() {
@@ -20,6 +19,7 @@ function RestoreColor() {
     var x = document.getElementById("rectangle");
     x.style.backgroundColor = "#0000ff";
 }
+
 function Hide() {
     var x = document.getElementById("rectangle");
 
@@ -31,3 +31,20 @@ function Reveal() {
 
     x.style.display = "block";        
 } 
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    var x = document.getElementById("rectangle");
+    var taille = x.offsetHeight; 
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        
+        x.style.height = taille + 100 + 'px';
+        sleep(1000);
+        x.style.height = taille - 100 + 'px';
+        
+        return}
+}
+
