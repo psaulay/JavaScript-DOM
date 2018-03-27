013c990a178da63cf88ba1b29e33c988bc56c3cb
maximejanod <maxime.j@codeur.online>
2018-03-27 14:52:41 +0200

    text-js

--- script.js
+++ script.js
@@ -31,3 +31,16 @@ function Reveal() {
 
     x.style.display = "block";        
 } 
+
+var textNode = document.getElementById("text");
+
+function fontSizer(){
+
+    if (textNode.style.fontSize== "20px"){
+       textNode.style.fontSize= "15px";
+    } else{
+        textNode.style.fontSize= "20px"
+    }
+}
+
+textNode.addEventListener("click",fontSizer);
